41f6c4e1bbd206ca00e684d0b27c55936875a54c
BekturMuratov <muratov225@gmail.com>
2022-01-04 22:10:39 +0600

    djikstra, matrix, breadthSearch

---
+++ poisk_v_shirinu.js
@@ -0,0 +1,27 @@
+const graph = {}
+
+graph.a = ['b','c']
+graph.b = ['f']
+graph.c = ['d', 'e']
+graph.d = ['c']
+graph.e = ['f']
+graph.f = ['g']
+
+
+function breadthSearch(graph,start,end){
+    let queue = []
+    queue.push(start)
+    while(queue.length > 0){
+        const current = queue.shift()
+        if(!graph[current]){
+            graph[current] = []
+        }
+        if(graph[current].includes(end)){
+            return true
+        }else {
+            queue = [...queue, ...graph[current]]
+        }
+    }
+}
+
+console.log(breadthSearch(graph, 'a', 'd'))
