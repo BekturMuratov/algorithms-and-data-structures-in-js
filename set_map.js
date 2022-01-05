0534e7f41e0ac0711694236afecd87f91f85754f
BekturMuratov <muratov225@gmail.com>
2022-01-05 16:09:01 +0600

    set and map

---
+++ set_map.js
@@ -0,0 +1,18 @@
+const map = new Map()
+
+map.set('name','Bektur Muratov')
+
+console.log(map.get('name'));
+
+
+const set = new Set()
+
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(5)
+set.add(4)
+set.add(3)
+set.add(2)
+
+console.log(set)
